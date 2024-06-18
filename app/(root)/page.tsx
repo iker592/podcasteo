f8d6bc2097e8739c9822f271ccd16d21237895c3
Iker Redondo <iker0592@gmail.com>
2024-06-18 11:58:49 +0000

    add assets and first use for logo. also favicon.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ const Home = ()=> {
   return (
     <div className="mt-9">
       <section className='flex flex-col gap-5'>
-        <h1  className='text-20 font-bold text-white-1'> Trending Podcasts</h1>+
+        <h1  className='text-20 font-bold text-white-1'> Trending Podcasts</h1>
         <Button className='text-white-1 bg-orange-1'>Button</Button>
       </section>
     </div>
